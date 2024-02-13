2be21797816c5581508b84de140a87bcd4c49754
julioCRV <julioroquevalda6@gmail.com>
2024-02-12 22:01:40 -0400

    nuevos cambios por problema en el deployado

--- djangoApi/settings.py
+++ djangoApi/settings.py
@@ -87,10 +87,9 @@ WSGI_APPLICATION = 'djangoApi.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(        # Feel free to alter this value to suit your needs.        
+    default='sqlite:///db.sqlite3',        
+    conn_max_age=600    )
 }
 
 
